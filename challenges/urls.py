f3dc434479a0c190099aaf18f88e455b1a66503b
AnnaKolmychek <a.kolmychek@gmail.com>
2024-12-15 03:33:07 +0300

    add base structure

---
+++ challenges/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from challenges.apps import ChallengesConfig
+
+
+app_name = ChallengesConfig.name
+
+urlpatterns = [
+    # path('', ___.as_view()),
+]
---
+++ progress/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from progress.apps import ProgressConfig
+
+
+app_name = ProgressConfig.name
+
+urlpatterns = [
+    # path('', ___.as_view()),
+]
