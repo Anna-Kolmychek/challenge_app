f65f17b4af65358b7b00f19771c51d177e4d38cd
AnnaKolmychek <a.kolmychek@gmail.com>
2025-02-02 14:44:16 +0300

    update deploy

--- config/settings.py
+++ config/settings.py
@@ -15,8 +15,6 @@ DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1").split(" ")
 
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
 
 INSTALLED_APPS = [
     'django.contrib.admin',
