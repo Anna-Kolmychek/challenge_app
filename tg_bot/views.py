e96dc18e0014b56ecb60a44aece1d091dd8baefb
AnnaKolmychek <a.kolmychek@gmail.com>
2025-02-15 17:11:17 +0300

    test auth

--- tg_bot/views.py
+++ tg_bot/views.py
@@ -2,9 +2,10 @@ import asyncio
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, generics
 
 from tg_bot.models import InitData
+from tg_bot.serializers import InitDataSerializer
 from tg_bot.tasks import send_tg_messages
 
 
@@ -14,3 +15,8 @@ class TestTGAuth(APIView):
         # asyncio.run(send_tg_messages())
         InitData.objects.create(text=header_auth)
         return Response(header_auth, status=status.HTTP_200_OK)
+
+
+class InitDataListAPIView(generics.ListAPIView):
+    queryset = InitData.objects.all()
+    serializer_class = InitDataSerializer
