8489261add58454016c3386bbad2078a7fcfebfd
AnnaKolmychek <a.kolmychek@gmail.com>
2025-02-22 19:11:52 +0300

    update tg auth

--- tg_bot/admin.py
+++ tg_bot/admin.py
@@ -1,1 +1,8 @@
 from django.contrib import admin
+
+from tg_bot.models import InitData
+
+
+@admin.register(InitData)
+class InitDataAdmin(admin.ModelAdmin):
+    pass
--- tg_bot/views.py
+++ tg_bot/views.py
@@ -4,10 +4,12 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
+from tg_bot.models import InitData
+
 
 class TestTGAuth(APIView):
     def get(self, request):
         header_auth = request.headers.get('authorization')
         # asyncio.run(send_tg_messages())
-        # InitData.objects.create(text=header_auth)
+        InitData.objects.create(text=header_auth)
         return Response(header_auth, status=status.HTTP_200_OK)
